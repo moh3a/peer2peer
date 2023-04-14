b07926524fe8969d7507068a9d0d71e2dff54e0d
moh3a <aitabdelmalekma@gmail.com>
2023-04-14 21:01:02 +0100

    video chat works

--- main.js
+++ main.js
@@ -25,21 +25,27 @@ let init = async () => {
   await channel.join();
 
   channel.on("MemberJoined", handleUserJoined);
+  channel.on("MemberLeft", handleUserLeft);
+  client.on("MessageFromPeer", handleMessageFromPeer);
 
+  await createLocalStream();
+};
+
+let createLocalStream = async () => {
   localStream = await navigator.mediaDevices.getUserMedia({
     video: true,
     audio: false, // todo: revert to true
   });
   document.getElementById("user-1").srcObject = localStream;
-
-  createOffer(); // todo: remove this
 };
 
-let createOffer = async () => {
+let createPeerConnection = async (MemberId) => {
   peerConnection = new RTCPeerConnection(servers);
   remoteStream = new MediaStream();
   document.getElementById("user-2").srcObject = remoteStream;
+  document.getElementById("user-2").style.display = "block";
 
+  if (!localStream) await createLocalStream();
   localStream.getTracks().forEach((track) => {
     peerConnection.addTrack(track, localStream);
   });
@@ -52,18 +58,80 @@ let createOffer = async () => {
 
   peerConnection.onicecandidate = async (event) => {
     if (event.candidate) {
-      console.log("new ice candidate", event.candidate);
+      client.sendMessageToPeer(
+        {
+          text: JSON.stringify({
+            type: "candidate",
+            data: event.candidate,
+          }),
+        },
+        MemberId
+      );
     }
   };
+};
 
+let createOffer = async (MemberId) => {
+  await createPeerConnection(MemberId);
   let offer = await peerConnection.createOffer();
   await peerConnection.setLocalDescription(offer);
 
-  console.log("offer", offer);
+  client.sendMessageToPeer(
+    { text: JSON.stringify({ type: "offer", data: offer }) },
+    MemberId
+  );
+};
+
+let createAnswer = async (MemberId, offer) => {
+  await createPeerConnection(MemberId);
+
+  await peerConnection.setRemoteDescription(offer);
+  let answer = await peerConnection.createAnswer();
+  await peerConnection.setLocalDescription(answer);
+
+  client.sendMessageToPeer(
+    { text: JSON.stringify({ type: "answer", data: answer }) },
+    MemberId
+  );
+};
+
+let addAnswer = (answer) => {
+  if (!peerConnection.currentRemoteDescription) {
+    peerConnection.setRemoteDescription(answer);
+  }
 };
 
 let handleUserJoined = async (MemberId) => {
-  console.log("a new user joined the channel", MemberId);
+  createOffer(MemberId);
 };
+
+let handleUserLeft = (MemberId) => {
+  document.getElementById("user-2").style.display = "none";
+};
+
+let handleMessageFromPeer = async (message, MemberId) => {
+  message = JSON.parse(message.text);
+
+  if (message.type === "offer") {
+    await createAnswer(MemberId, message.data);
+  }
+
+  if (message.type === "answer") {
+    addAnswer(message.data);
+  }
+
+  if (message.type === "candidate") {
+    if (peerConnection) {
+      peerConnection.addIceCandidate(message.data);
+    }
+  }
+};
+
+let leaveChannel = async () => {
+  await channel.leave();
+  await client.logout();
+};
+
+window.addEventListener("beforeunload", leaveChannel);
 
 init();
